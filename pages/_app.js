1f32887466962f95b01f1a57f216f375c11c464e
Ashley King <ashleyot@gmail.com>
2019-07-21 10:17:08 -0400

    material-ui integrated with next; styles rendering server-side and removed with component mounts

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import App, { Container } from 'next/app';
 import Header from './components/Header';
+import theme from '../lib/theme';
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -13,13 +16,27 @@ class MyApp extends App {
     return { pageProps };
   }
 
+  componentDidMount() {
+    // Remove the server-side injected styles.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const { Component, pageProps } = this.props;
 
     return (
       <Container>
-        <Header {...pageProps} />
-        <Component {...pageProps} />
+        {/* ThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Header {...pageProps} />
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Container>
     );
   }
