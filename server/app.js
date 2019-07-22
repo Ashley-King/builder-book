66da14a15e6819d1ea5d62af0f2f5bfb131e9039
Ashley King <ashleyot@gmail.com>
2019-07-22 07:42:34 -0400

    added session

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const next = require('next');
-const mongoose = require('mongoose');
 require('dotenv').config();
+
+const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoSessionStore = require('connect-mongo');
+
 const User = require('./models/User');
 
 const dev = process.env.NODE_ENV !== 'production';
@@ -23,9 +27,25 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
-
+  const MongoStore = mongoSessionStore(session);
+  const sess = {
+    name: 'builderbook.sid',
+    secret: 'HD2w.)q*VqRT4/#NK2M/,E^B)}FED5fWU!dKe[wk',
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 14 * 24 * 60 * 60, // save session 14 days
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+    },
+  };
+  server.use(session(sess));
   server.get('/', async (req, res) => {
     // const user = { email: 'team@builderbook.org' };
+    req.session.foo = 'bar';
     const user = await User.findOne({ slug: 'team-builder-book' });
     app.render(req, res, '/', { user });
   });
